b1f616fdb6824053e978d503d464a1a8629a0e0a
65070188Mananchaya <97225144+DraftManan@users.noreply.github.com>
2025-02-11 00:13:12 +0700

    create footer

---
+++ movie-booking/app/components/Footer.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+const Footer = () => {
+    return (
+      <footer className="bg-black text-white mt-20 py-4">
+        
+
+  
+        <div className="text-center text-xs text-gray-500">
+          Copyright © {new Date().getFullYear()} MovieGO. All Rights Reserved.
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
